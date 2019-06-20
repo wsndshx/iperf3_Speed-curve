c79b5699924705edeb2b7b2aa767534e510d71d1
zzlwd <1281097446@qq.com>
2019-06-20 15:40:43 +0800

    继续适配输出的日志文件

--- iperf3.sh
+++ iperf3.sh
@@ -5,9 +5,13 @@ echo "正在处理文件：$1"
 #开始预处理文件内容
 sed -i '/^iperf3/'d $1 #删除以iperf3开头的行
 sed -i '/^Connecting/'d $1 #删除以Connecting开头的行
+sed -i '/^-/'d $1
 sed -i '/Bandwidth$/'d $1 #删除以Bandwidth结尾的行
 sed -i '/Cwnd$/'d $1
 sed -i '/5201$/'d $1
+sed -i '/Retr$/'d $1
+sed -i '/sender$/'d $1
+sed -i '/receiver$/'d $1
 awk -F'MBytes' -vOFS="MBytes" '{$1="";$1=$1}1' $1 > $1.tmp #删除MBytes前面的所有东西
 mv -f $1.tmp $1
 rm -f $1.tmp
@@ -21,21 +25,21 @@ sed -i '/^$/d' $1 #删除空白行
 nl -n ln $1 > $1.tmp
 mv -f $1.tmp $1
 rm -f $1.tmp
-sed -i 's/     //g' $1
-sed -i 's/    //g' $1
-sed -i 's/   //g' $1
-sed -i 's/  //g' $1
-sed -i 's/	/ /g' $1
+sed -i 's/  */ /g' $1 #将多余的空格变为一个
 #确定表格的宽
-kuan="$3*$4" | bc
-#开始生成表格
+kuan="$3/$4" | bc
+#确定表格横坐标最大值
+hanshu= cat $1 | wc --lines | sed -e 's/$1//g' -e 's/ //g'
+echo "$hanshu"
+开始生成表格
 gnuplot -persist <<-EOFMarker
 	set title "$2"
 	set xlabel "time/s"
 	set ylabel "Mbits/s"
 	set grid
+	set xrange [1:$hanshu]
 	set terminal pngcairo size $3,$kuan color solid linewidth 2 font "Helvetica, 30"
-	set size ratio 0.25
+	set size ratio $4
 	set output "$2.png"
 	plot "$1" with linespoints
 EOFMarker
